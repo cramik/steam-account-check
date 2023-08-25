5bcebcc2307a7fca3767766f1742ed4edc7a9c64
cramik <5573629+cramik@users.noreply.github.com>
2023-08-25 00:30:32 -0700

    Fixed big bugs
    Apparently I mixed up 'message_sessions' as 'manage_sessions' so the chat wasn't working this entire time. I only realized because I logged into an account and saw there were chats there. Also added a check to make sure the request for chat messages goes through to prevent exception as well as moved the timestamp updating to inside that check in case it fails

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ for account in config['accounts']:
 	sessions_dict=proto_to_dict(sessions_req.body)
 	total_chats = 0
 	total_messages = 0
-	if 'manage_sessions' in sessions_dict:
+	if 'message_sessions' in sessions_dict:
 		account_ids = [session['accountid_friend'] for session in sessions_dict['message_sessions'] if session['last_message']>account['timestamp']]
 		for account_id in account_ids:
 			total_chats+=1
@@ -49,13 +49,15 @@ for account in config['accounts']:
 			sender_name = sender_info['response']['players'][0]['personaname']
 			sender_avatar = sender_info['response']['players'][0]['avatar']
 			recent_messages_req=client.send_um_and_wait("FriendMessages.GetRecentMessages#1", {'steamid1':client.steam_id.as_64,'steamid2':SteamID(account_id).as_64})
-			recent_messages_dict=proto_to_dict(recent_messages_req.body)
-			for message in recent_messages_dict['messages'][::-1]:
-				if (message['timestamp']>account['timestamp']) and (message['accountid']!=client.steam_id.id):
-					total_messages+=1
-					requests.post(config['webhook'], json={"content": message['message'], "username": f"{sender_name} ({account['username']})","avatar_url":sender_avatar})
+			if recent_messages_req is not None: #Ocassionally it is none, not sure if this is because the request failed or if the chat is too old
+				recent_messages_dict=proto_to_dict(recent_messages_req.body)
+				for message in recent_messages_dict['messages'][::-1]:
+					if (message['timestamp']>account['timestamp']) and (message['accountid']!=client.steam_id.id):
+						total_messages+=1
+						requests.post(config['webhook'], json={"content": message['message'], "username": f"{sender_name} ({account['username']})","avatar_url":sender_avatar})
+		account['timestamp']=sessions_dict['timestamp']
 	LOG.info("Account %s had: %d friend requests and %d messages across %d chats", account['username'], len([x for x in client.friends if x.relationship==2]), total_messages, total_chats)
-	account['timestamp']=sessions_dict['timestamp']
+	
 	try:
 		with open("config.json", 'w') as config_file: json.dump(config,config_file,indent=2)
 	except:
